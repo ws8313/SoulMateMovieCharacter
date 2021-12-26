43945d44b1ecf35dce12e6401804c2fa182e9e69
hyeminKim <hmkim199@naver.com>
2021-12-26 20:32:46 +0900

    feat: add .gitignore

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    # local에서는 port 5000 debug True로 하기
+    # 배포시에 port 80 debug False로!
+    app.run('0.0.0.0', 80, debug=False)
