6fa8fcbd6971c719186c16f3096c55839d66acfc
hyeminKim <hmkim199@naver.com>
2021-12-26 20:37:54 +0900

    test: test server

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# if __name__ == '__main__':
+#     # local에서는 port 5000 debug True로 하기
+#     # 배포시에 port 80 debug False로!
+#     app.run('0.0.0.0', 5000, debug=True)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
 if __name__ == '__main__':
-    # local에서는 port 5000 debug True로 하기
-    # 배포시에 port 80 debug False로!
-    app.run('0.0.0.0', 80, debug=False)
+    app.run()
