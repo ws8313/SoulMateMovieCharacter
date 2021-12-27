dde88108818ee9d042a8db89fb56bd69b106ca0a
ws8313 <ws8313@naver.com>
2021-12-27 18:27:06 +0900

    feat: 리액트 재설치 및 메인페이지 베이스 구현

---
+++ src/components/MainPage.js
@@ -0,0 +1,31 @@
+import React, {} from "react";
+
+const TestPage = ({ history }) => {
+
+    return (
+        <div>
+            <div>
+                <p>일리스</p>
+            </div>
+
+            <div>
+                <div>이미지</div>
+            </div>
+
+            <div>
+                <p>코로나 시국에..</p>
+                <p>이런게 나의 영화 인생캐 일리가...!!</p>
+            </div>
+            
+            <div>
+                <button type="submit">인생캐 알아보기</button>
+            </div>
+
+            <div>
+                <button type="submit">바로 결과 보기</button>
+            </div>
+        </div>
+    )
+}
+
+export default TestPage;
