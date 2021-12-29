fff2406f85aa6398efd601dc189d685e39953304
hyeminKim <hmkim199@naver.com>
2021-12-29 14:53:06 +0900

    feat: calculate mbti & store user mbti data

--- views/main_views.py
+++ views/main_views.py
@@ -1,18 +1,19 @@
 from flask import Blueprint
 from flask import request, jsonify, g, session, render_template
 from models import *
+from collections import Counter
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-# @bp.before_app_request
-# def load_logged_in_user():
-#     user_email = session.get('login')
-#     if login is None:
-#         g.user = None
-#     else:
-#         g.user = db.session.query(User).filter(User.email == user_email).first()
-
+@bp.before_app_request
+def load_logged_in_user():
+    # user_email = session.get('login')
+    # if login is None:
+    #     g.user = None
+    # else:
+    #     g.user = db.session.query(User).filter(User.email == user_email).first()
+    g.user = "test"
 
 @bp.route('/hello')
 def hello():
@@ -38,16 +39,39 @@ def result():
     # answers가 ["a", "b", ..."] 형태 리스트 형태로 전달된다고 가정.
     else:
         answers = request.form.getlist('answers')
-        print(answers)
-        for i in range(len(answers)):
-            option = db.session.query(Option.mbti_indicator).filter(Option.question_id == i+1, Option.content.like(answers[i]+'%')).first()
-            option = str(getattr(option, Option.mbti_indicator.name))
-            # TODO: mbti 계산
-            
-        # 리스트를 문자열로 변환
-        # answers = "".join(str(_) for _ in answers)
+        # print(answers)
 
-        # answer = Answer(g.user, answers)
-        # db.session.add(answer)
-        # db.session.commit()
+        result = [[], [], [], []]
+        for i in range(len(answers)):
+            mbti_indicator = db.session.query(Option.mbti_indicator).filter(Option.question_id == i+1, Option.content.like(answers[i]+'%')).first()
+            mbti_indicator = str(getattr(mbti_indicator, Option.mbti_indicator.name))
+            
+            if mbti_indicator in ('I', 'E'):
+                result[0].append(mbti_indicator)
+            elif mbti_indicator in ('N', 'S'):
+                result[1].append(mbti_indicator)
+            elif mbti_indicator in ('T', 'F'):
+                result[2].append(mbti_indicator)
+            elif mbti_indicator in ('P', 'J'):
+                result[3].append(mbti_indicator)
+            else:
+                print('잘못된 데이터입니다.')
+                return
+        
+        # mbti 계산            
+        user_mbti = ""
+        for li in result:
+            user_mbti += Counter(li).most_common(n=1)[0][0]
+        # print(user_mbti)
+        
+        # mbti 저장
+        user = User.query.filter(User.id == g.user).first()
+        user.mbti = user_mbti
+
+        # 리스트를 문자열로 변환
+        answers = "".join(str(_) for _ in answers)
+
+        answer = Answer(g.user, answers)
+        db.session.add(answer)
+        db.session.commit()
         return jsonify({"result": "success"})
