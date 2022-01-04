e377fefaacd070d1e6cbce09d243c1a8c6698988
ss.k <kos7114@naver.com>
2022-01-05 01:22:34 +0900

    feat: mbti matching character

--- backend/views/character.py
+++ backend/views/character.py
@@ -17,6 +17,7 @@ def load_user(user_id):
     return User.query.filter(User.id == user_id).first()
 
 @MbtiCharacter.route('/<string:mbti>')
+# 현재 current_user.id 값을 확인하지 못하는 문제로 test를 참고해 우선 mbti값을 받아서 그 mbti를 가지는 캐릭터와 이미지를 가져오도록 했다. 이후 frontpage를 확인 받고 나면 그페이지에서 mbti값을 받아오지 않고도 백엔드 혼자서 current_id를 통해 값을 전송해 줄 수 있도록 할 예정이다.
 @MbtiCharacter.doc(params={'mbti': '검색할 mbti'})
 class ShowCharacter(Resource):
     @MbtiCharacter.response(200, 'Success', matching_fields)
