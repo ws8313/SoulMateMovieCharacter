6a279cf18e9369ca027a1ac19709e56d00f53ffe
hyeminKim <hmkim199@naver.com>
2022-01-05 20:32:58 +0900

    feat: working on showing all movies about character

--- backend/views/character.py
+++ backend/views/character.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from flask_restx import Resource, Namespace, fields
 from flask_restx.marshalling import marshal
 from models import *
 from flask_login import current_user
 from app import login_manager
+import json
 
 MbtiCharacter = Namespace(
     name='MbtiCharacter',
@@ -10,18 +13,41 @@ MbtiCharacter = Namespace(
 )
 
 matching_fields = MbtiCharacter.model('Mbti Character', {
+    'character_id': fields.List(fields.Integer),
     'character_name': fields.List(fields.String),
     'character_image': fields.List(fields.String)
 })
 
-character_N_movies_fields = MbtiCharacter.model('Character and Movies', {
-    'character_name': fields.String(description="캐릭터 이름"),
-    'character_movie_list': fields.List(fields.String)
+movie_info_fields = MbtiCharacter.model('Movie info', {
+    'id': fields.Integer(description="movie id"),
+    'kor_title': fields.String,
+    'eng_title': fields.String,
+    'image_link': fields.String,
+    'pub_year': fields.Integer,
+    'director': fields.String,
+    'rating': fields.Float,
+    'story': fields.String,
+    'run_time': fields.Integer,
+    'genres': fields.List(fields.String)
 })
 
-character_N_movies_list_fields = MbtiCharacter.model('Characters and Movies List', {
-    'character_N_movies_list': fields.List(fields.Raw(character_N_movies_fields))
+character_N_movies_fields = MbtiCharacter.model('Character and Movies', {
+    'character_name': fields.String(description="캐릭터 이름"),
+    'movies': fields.List(fields.Raw(movie_info_fields))
 })
+
+total_character_N_movies_fields = MbtiCharacter.model('Characters and Movies List', {
+    'total_character_N_movies': fields.List(fields.Raw(character_N_movies_fields))
+})
+
+
+def row2dict(row):
+    dictionary = {}
+    for column in row.__table__.columns:
+        dictionary[column.name] = str(getattr(row, column.name))
+
+    return dictionary
+
 
 # 공통 코드를 함수화 시켜 따로 보관, mbti에 맞는 캐릭터를 출력
 def ShowCharacter(mbti):
@@ -59,13 +85,13 @@ class CompatibleMbti(Resource):
         return ShowCharacter(compatible_mbti.compatible_mbti)
 
 
-# #테스트용 코드
-# @MbtiCharacter.route('/<string:mbti>')
-# @MbtiCharacter.doc(params={'mbti': '검색할 mbti'})
-# class UserCharacter(Resource):
-#     @MbtiCharacter.response(200, 'Success', matching_fields)
-#     def get(self, mbti):
-#         return ShowCharacter(mbti)
+#테스트용 코드
+@MbtiCharacter.route('/<string:mbti>')
+@MbtiCharacter.doc(params={'mbti': '검색할 mbti'})
+class UserCharacter(Resource):
+    @MbtiCharacter.response(200, 'Success', matching_fields)
+    def get(self, mbti):
+        return ShowCharacter(mbti)
 
 # @MbtiCharacter.route('/compatibility/<string:mbti>')
 # @MbtiCharacter.doc(params={'mbti': '검색할 mbti'})
@@ -78,7 +104,7 @@ class CompatibleMbti(Resource):
 @MbtiCharacter.route('/movie_list/<string:mbti>')
 @MbtiCharacter.doc(params={'mbti': '등장한 영화 리스트를 볼 캐릭터의 mbti'})
 class MovieListWithCharacters(Resource):
-    @MbtiCharacter.response(200, 'Success', character_N_movies_list_fields)
+    @MbtiCharacter.response(200, 'Success', total_character_N_movies_fields)
     @MbtiCharacter.response(500, 'fail')
     def get(self, mbti):
         """mbti 에 해당하는 캐릭터가 등장한 영화 리스트"""
@@ -87,22 +113,24 @@ class MovieListWithCharacters(Resource):
         characters = list(ShowCharacter(mbti))
         for c in characters:
             # 영화 리스트 검색
-            character = []
-            character.append("캐릭터 이름")# c[1] : 캐릭터 이름
+            character = {}
+            character['character_name'] = "캐릭터 이름" # c[1] : 캐릭터 이름
             movie_list = db.session.query(CharacterInMovie.movie_id).filter(CharacterInMovie.character_id == 1) # c[0] : 캐릭터 id
 
             movie_infos = []
             for row in movie_list:
-                movie_info = db.session.query(Movie).filter(Movie.id == row.movie_id).all()
+                movie_info = db.session.query(Movie).filter(Movie.id == row.movie_id).first()
+                temp_dict = row2dict(movie_info)
+                genres = db.session.query(MovieGenre.genre).filter(MovieGenre.movie_id == row.movie_id).all()
+                genres = [str(getattr(g, MovieGenre.genre.name)) for g in genres]
+                temp_dict['genres'] = genres
+                movie_infos.append(temp_dict)
 
-                print(movie_info)
-
-            movie_list = [row.movie_id for row in movie_list.all()]
-            character['character_movie_list'] = movie_list
+            character['movies'] = movie_infos
             character_N_movie_list.append(character)
         
-        # print(character_N_movie_list)
+        # print(json.dumps(character_N_movie_list, ensure_ascii=False, indent=4))
         
         return {
-            'character_N_movies_list': character_N_movie_list
+            'total_character_N_movies': character_N_movie_list
         }
