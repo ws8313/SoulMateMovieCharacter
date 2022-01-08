40ccc8b4e6ef05d6ca04998c040ddd20dd1d60ae
ws8313 <ws8313@naver.com>
2022-01-09 07:17:11 +0900

    test

--- backend/views/user.py
+++ backend/views/user.py
@@ -57,7 +57,7 @@ class Logout(Resource):
         return {'result': 'success'}, 200
 
 
-@UserManagement.route('/register', methods=['GET', 'POST'])
+@UserManagement.route('/register', methods = ['POST'])
 class Register(Resource):
     @UserManagement.expect(register_fields)
     @UserManagement.response(200, 'success')
