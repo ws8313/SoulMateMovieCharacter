d3f4d58cb446cedc0f98bf9f14f08355c9ebee89
ws8313 <ws8313@naver.com>
2022-01-09 10:55:34 +0900

    test

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -21,7 +21,7 @@ const LoginModal = ({ openLoginModal }) => {
 
     const onSubmit = () => {
         axios
-            .post("/user/login", {
+            .get("/user/login", {
                 "login_id": userId,
                 "login_pw": password,
             }, axiosConfig)
