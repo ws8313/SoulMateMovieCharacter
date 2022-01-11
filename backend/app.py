77a6df90bb3685f809783b09677859c9f5303d14
hyeminKim <hmkim199@naver.com>
2022-01-11 18:04:02 +0900

    fix: cors origin url

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ login_manager = LoginManager()
 # flask run으로 실행해야 함.
 def create_app():
     app = Flask(__name__)
-    cors = CORS(app, resources={r"*": {"origins": ["elice-kdt-3rd-team-12.koreacentral.cloudapp.azure.com"]}},
+    cors = CORS(app, resources={r"*": {"origins": ["http://elice-kdt-3rd-team-12.koreacentral.cloudapp.azure.com"]}},
         supports_credentials=True,)
     app.config.from_object(config)
     login_manager.init_app(app)
