ba8eaa2b82fe9b360a42e6a1a81746a2af141dd3
hmkim199 <hmkim199@gmail.com>
2022-01-25 20:54:14 +0900

    docs: add daily scrum meeting template

---
+++ backend/data/delete_null_image.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from app import db
+from models import *
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import json
+
+def deleteNullImage():
+    with open('./data/real/mbti.json', 'r', encoding="utf-8") as f:
+        characters = json.load(f)
+        null_image_url = "https://www.personality-database.com/images/profile_transparent.png"
+        db.session.query(Character).filter(Character.image_link == null_image_url).delete()                
+    
+    db.session.commit()
