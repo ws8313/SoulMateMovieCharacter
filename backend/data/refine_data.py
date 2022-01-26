d25c6f2f027ae34fde546d64daa0a315e9f43d76
hmkim199 <hmkim199@gmail.com>
2022-01-27 02:21:21 +0900

    fix: delete html entity

--- backend/data/refine_data.py
+++ backend/data/refine_data.py
@@ -1,9 +1,8 @@
 import os
 import sys
 from app import db
-from models import *
+from models import Movie, Character
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-import json
 
 def delete_null_image_in_DB():
     null_image_url = "https://www.personality-database.com/images/profile_transparent.png"
@@ -11,5 +10,17 @@ def delete_null_image_in_DB():
     db.session.commit()
 
 
-# def delete_html_entity():
-    # db.session.query(Movie).filter(Movie.)
+def delete_html_entity():
+    kor_titles = db.session.query(Movie).filter(Movie.kor_title.like('%&amp;%')).all()
+    eng_titles = db.session.query(Movie).filter(Movie.eng_title.like('%&amp;%')).all()
+    
+    for kor_title in kor_titles:
+        title = kor_title.kor_title
+        title = title.replace('&amp;', '&')
+        kor_title.kor_title = title
+
+    for eng_title in eng_titles:
+        title = eng_title.eng_title
+        title = title.replace('&amp;', '&')
+        eng_title.eng_title = title
+    db.session.commit()
