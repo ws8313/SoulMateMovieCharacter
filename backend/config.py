8b05d4efbb1155368a22c56f7abbf6f5b345f351
ws8313 <ws8313@naver.com>
2022-06-28 23:13:22 +0900

    file directory

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'database.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = os.urandom(12).hex()
