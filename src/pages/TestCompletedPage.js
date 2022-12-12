c563c9dadbf70e95329dda419b95c7ab60bf800c
Wooseong Kim <kimwooseong8313@gmail.com>
2022-12-12 21:52:57 +0900

    refactor: TestCompletedPage code formatting

--- src/pages/TestCompletedPage.js
+++ src/pages/TestCompletedPage.js
@@ -5,51 +5,80 @@ import prevbtn from "../img/prevbtn.png";
 import styles from "./TestCompletedPage.module.css";
 
 const TestCompletedPage = () => {
-    const [userMBTI, setUserMBTI] = useState("")
+  const [userMBTI, setUserMBTI] = useState("");
 
-    const history = useHistory();
+  const history = useHistory();
 
-    useEffect(() => {
-        async function getMBTI() {
-            try {
-                const res = await axios.get("http://localhost:5000/result/", {withCredentials: true})
-                setUserMBTI(res.data.user_mbti)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        getMBTI();
-    }, [userMBTI]);
-    
-    return (
-        <div id={styles.container}>
-            <div id={styles.btnbox} onClick={ () => { history.goBack() }}>
-                <img className={styles.prevbtn} src={prevbtn} alt="prevbtn" />
-            </div>
+  useEffect(() => {
+    async function getMBTI() {
+      try {
+        const res = await axios.get(
+          "https://soulmatemoviecharacter-ws8313.koyeb.app/result/",
+          { withCredentials: true }
+        );
+        setUserMBTI(res.data.user_mbti);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getMBTI();
+  }, [userMBTI]);
 
-            <div className={styles.title}>
-                <p>일리스</p>
-            </div>
+  return (
+    <div id={styles.container}>
+      <div
+        id={styles.btnbox}
+        onClick={() => {
+          history.goBack();
+        }}
+      >
+        <img className={styles.prevbtn} src={prevbtn} alt="prevbtn" />
+      </div>
 
-            <div id={styles.divider}></div>
+      <div className={styles.title}>
+        <p>일리스</p>
+      </div>
 
-            <div>
-                <p className={styles.text}>테스트가 완료되었습니다</p>
-            </div>
-            
-            <div>
-                <button className={styles.btn1} onClick={ () => { history.push("/MbtiCharacterPage") }}>나와 같은 유형인 <br /> 캐릭터 확인하기</button>
-            </div>
+      <div id={styles.divider}></div>
 
-            <div>
-                <button className={styles.btn2} onClick={ () => { history.push("/MbtiCompatiblePage") }}>나와 궁합이 잘 맞는 <br /> 캐릭터 확인하기</button>
-            </div>
+      <div>
+        <p className={styles.text}>테스트가 완료되었습니다</p>
+      </div>
 
-            <div>
-                <button className={styles.btn3} onClick={ () => { history.push("/MbtiTop10Page") }}>{userMBTI} 유형 관련 인기있는 <br /> 영화 확인하기</button>
-            </div>
-        </div>
-    )
-}
+      <div>
+        <button
+          className={styles.btn1}
+          onClick={() => {
+            history.push("/MbtiCharacterPage");
+          }}
+        >
+          나와 같은 유형인 <br /> 캐릭터 확인하기
+        </button>
+      </div>
+
+      <div>
+        <button
+          className={styles.btn2}
+          onClick={() => {
+            history.push("/MbtiCompatiblePage");
+          }}
+        >
+          나와 궁합이 잘 맞는 <br /> 캐릭터 확인하기
+        </button>
+      </div>
+
+      <div>
+        <button
+          className={styles.btn3}
+          onClick={() => {
+            history.push("/MbtiTop10Page");
+          }}
+        >
+          {userMBTI} 유형 관련 인기있는 <br /> 영화 확인하기
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default TestCompletedPage;
