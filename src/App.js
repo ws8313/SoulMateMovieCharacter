b2c9861b9a1fea7c9bfcfe6ef74c32317c76efd6
Wooseong Kim <kimwooseong8313@gmail.com>
2022-12-25 18:03:10 +0900

    chore: App.js code formatting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import MainPage from './pages/MainPage';
-import TestPage from './pages/TestPage';
-import TestCompletedPage from './pages/TestCompletedPage';
-import MbtiCharacterPage from './pages/MbtiCharacterPage';
-import MbtiCharacterMovieListPage from './pages/MbtiCharacterMovieListPage';
-import MovieInfoPage from './pages/MovieInfoPage';
-import MbtiCompatiblePage from './pages/MbtiCompatiblePage';
-import MbtiCompatibleMovieListPage from './pages/MbtiCompatibleMovieListPage';
-import MbtiTop10Page from './pages/MbtiTop10Page';
-
+import MainPage from "./pages/MainPage";
+import TestPage from "./pages/TestPage";
+import TestCompletedPage from "./pages/TestCompletedPage";
+import MbtiCharacterPage from "./pages/MbtiCharacterPage";
+import MbtiCharacterMovieListPage from "./pages/MbtiCharacterMovieListPage";
+import MovieInfoPage from "./pages/MovieInfoPage";
+import MbtiCompatiblePage from "./pages/MbtiCompatiblePage";
+import MbtiCompatibleMovieListPage from "./pages/MbtiCompatibleMovieListPage";
+import MbtiTop10Page from "./pages/MbtiTop10Page";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={ MainPage } />
-        <Route path="/TestPage" component={ TestPage } />
-        <Route path="/TestCompletedPage" component={ TestCompletedPage } />
-        <Route path="/MbtiCharacterPage" component={ MbtiCharacterPage } />
-        <Route path="/MbtiCharacterMovieListPage" component={ MbtiCharacterMovieListPage } />
-        <Route path="/MovieInfoPage" component={ MovieInfoPage } />
-        <Route path="/MbtiCompatiblePage" component={ MbtiCompatiblePage } />
-        <Route path="/MbtiCompatibleMovieListPage" component={ MbtiCompatibleMovieListPage } />
-        <Route path="/MbtiTop10Page" component={ MbtiTop10Page } />
+        <Route exact path="/" component={MainPage} />
+        <Route path="/TestPage" component={TestPage} />
+        <Route path="/TestCompletedPage" component={TestCompletedPage} />
+        <Route path="/MbtiCharacterPage" component={MbtiCharacterPage} />
+        <Route
+          path="/MbtiCharacterMovieListPage"
+          component={MbtiCharacterMovieListPage}
+        />
+        <Route path="/MovieInfoPage" component={MovieInfoPage} />
+        <Route path="/MbtiCompatiblePage" component={MbtiCompatiblePage} />
+        <Route
+          path="/MbtiCompatibleMovieListPage"
+          component={MbtiCompatibleMovieListPage}
+        />
+        <Route path="/MbtiTop10Page" component={MbtiTop10Page} />
       </Switch>
     </div>
   );
