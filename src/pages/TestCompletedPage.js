4d235aaaedb3d316fe0568acc59f813d39e9b053
Wooseong Kim <kimwooseong8313@gmail.com>
2022-12-27 19:25:28 +0900

    refactor: TestCompletedPage

--- src/pages/TestCompletedPage.js
+++ src/pages/TestCompletedPage.js
@@ -49,14 +49,10 @@ const TestCompletedPage = () => {
   }, [userMBTI]);
 
   return (
-    <div id={styles.container}>
-      <div className={styles.title}>
-        <div>영화 캐릭터 테스트</div>
-      </div>
+    <div className={styles.container}>
+      <div className={styles.title}>영화 캐릭터 테스트</div>
 
-      <div id={styles.subtitle}>
-        <div>테스트가 완료되었습니다</div>
-      </div>
+      <div className={styles.subtitle}>테스트가 완료되었습니다</div>
 
       <div className={styles.img_container}>
         <img
@@ -67,49 +63,41 @@ const TestCompletedPage = () => {
         <div className={styles.text}>{userMBTI} 유형의 워드 클라우드</div>
       </div>
 
-      <div>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            history.push("/MbtiCharacterPage");
-          }}
-        >
-          나와 같은 유형인 캐릭터 확인하기
-        </button>
-      </div>
+      <button
+        className={styles.btn}
+        onClick={() => {
+          history.push("/MbtiCharacterPage");
+        }}
+      >
+        나와 같은 유형인 캐릭터 확인하기
+      </button>
 
-      <div>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            history.push("/MbtiCompatiblePage");
-          }}
-        >
-          나와 궁합이 잘 맞는 캐릭터 확인하기
-        </button>
-      </div>
+      <button
+        className={styles.btn}
+        onClick={() => {
+          history.push("/MbtiCompatiblePage");
+        }}
+      >
+        나와 궁합이 잘 맞는 캐릭터 확인하기
+      </button>
 
-      <div>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            history.push("/MbtiTop10Page");
-          }}
-        >
-          같은 {userMBTI} 유형에게 인기있는 영화 확인하기
-        </button>
-      </div>
+      <button
+        className={styles.btn}
+        onClick={() => {
+          history.push("/MbtiTop10Page");
+        }}
+      >
+        같은 {userMBTI} 유형에게 인기있는 영화 확인하기
+      </button>
 
-      <div>
-        <button
-          className={styles.btn}
-          onClick={() => {
-            history.push("/");
-          }}
-        >
-          테스트 다시 하기
-        </button>
-      </div>
+      <button
+        className={styles.btn}
+        onClick={() => {
+          history.push("/");
+        }}
+      >
+        테스트 다시 하기
+      </button>
     </div>
   );
 };
