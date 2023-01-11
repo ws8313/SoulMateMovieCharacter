371a82a4c5b1c13138311c9a8cfc7d6dfad58f97
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-11 20:31:28 +0900

    feat: Main components add subtitle

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Main.module.css";
 
-const Main = ({ src, alt, description }) => {
+const Main = ({ src, alt, subtitle, description }) => {
   return (
     <div>
       <div className={styles.img_container}>
         <img className={styles.content_img} src={src} alt={alt} />
+        {subtitle && <div className={styles.subtitle}>{subtitle}</div>}
       </div>
 
       {description && (
