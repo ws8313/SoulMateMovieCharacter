da4fff3531d625235f5089a5089b1d8af98beecb
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-12 22:45:04 +0900

    feat: components import index

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./Button/Button";
+export { default as Header } from "./Header/Header";
+export { default as Loading } from "./Loading/Loading";
+export { default as Main } from "./Main/Main";
+export { default as Modal } from "./Modal/Modal";
+export { default as LoginModal } from "./Modal/LoginModal";
+export { default as ProgressBar } from "./ProgressBar/ProgressBar";
