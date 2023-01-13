1c42eb2c3c50d6ddae1b375a945669138d0fdcc3
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-13 22:03:28 +0900

    refactor: App add lazy, suspence

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Switch } from "react-router-dom";
+import { Loading } from "./components";
 
-import MainPage from "./pages/MainPage";
-import TestPage from "./pages/TestPage";
-import TestCompletedPage from "./pages/TestCompletedPage";
-import MbtiCharacterPage from "./pages/MbtiCharacterPage";
-import MbtiCharacterMovieListPage from "./pages/MbtiCharacterMovieListPage";
-import MovieInfoPage from "./pages/MovieInfoPage";
-import MbtiCompatiblePage from "./pages/MbtiCompatiblePage";
-import MbtiCompatibleMovieListPage from "./pages/MbtiCompatibleMovieListPage";
-import MbtiTop10Page from "./pages/MbtiTop10Page";
+const MainPage = lazy(() => import("./pages/MainPage"));
+const TestPage = lazy(() => import("./pages/TestPage"));
+const TestCompletedPage = lazy(() => import("./pages/TestCompletedPage"));
+const MbtiCharacterPage = lazy(() => import("./pages/MbtiCharacterPage"));
+const MbtiCharacterMovieListPage = lazy(() =>
+  import("./pages/MbtiCharacterMovieListPage")
+);
+const MbtiCompatiblePage = lazy(() => import("./pages/MbtiCompatiblePage"));
+const MbtiCompatibleMovieListPage = lazy(() =>
+  import("./pages/MbtiCompatibleMovieListPage")
+);
+const MbtiTop10Page = lazy(() => import("./pages/MbtiTop10Page"));
+const MovieInfoPage = lazy(() => import("./pages/MovieInfoPage"));
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={MainPage} />
-        <Route path="/TestPage" component={TestPage} />
-        <Route path="/TestCompletedPage" component={TestCompletedPage} />
-        <Route path="/MbtiCharacterPage" component={MbtiCharacterPage} />
-        <Route
-          path="/MbtiCharacterMovieListPage"
-          component={MbtiCharacterMovieListPage}
-        />
-        <Route path="/MovieInfoPage" component={MovieInfoPage} />
-        <Route path="/MbtiCompatiblePage" component={MbtiCompatiblePage} />
-        <Route
-          path="/MbtiCompatibleMovieListPage"
-          component={MbtiCompatibleMovieListPage}
-        />
-        <Route path="/MbtiTop10Page" component={MbtiTop10Page} />
-      </Switch>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/TestPage" component={TestPage} />
+          <Route path="/TestCompletedPage" component={TestCompletedPage} />
+          <Route path="/MbtiCharacterPage" component={MbtiCharacterPage} />
+          <Route
+            path="/MbtiCharacterMovieListPage"
+            component={MbtiCharacterMovieListPage}
+          />
+          <Route path="/MovieInfoPage" component={MovieInfoPage} />
+          <Route path="/MbtiCompatiblePage" component={MbtiCompatiblePage} />
+          <Route
+            path="/MbtiCompatibleMovieListPage"
+            component={MbtiCompatibleMovieListPage}
+          />
+          <Route path="/MbtiTop10Page" component={MbtiTop10Page} />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
