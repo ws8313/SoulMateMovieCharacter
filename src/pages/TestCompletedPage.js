bba19c08ab22d80be181bb2aa4b5bff101ada5e4
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-18 18:13:20 +0900

    refactor: TestCompletedPage

--- src/pages/TestCompletedPage.js
+++ src/pages/TestCompletedPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import styles from "./TestCompletedPage.module.css";
+import { Header, Main, Button } from "../components";
 
 const TestCompletedPage = () => {
   const [userMBTI, setUserMBTI] = useState("");
@@ -24,7 +25,8 @@ const TestCompletedPage = () => {
     async function getMBTI() {
       try {
         const res = await axios.get(
-          "https://soulmatemoviecharacter-ws8313.koyeb.app/result/",
+          "http://localhost:5000/result/",
+          // "https://soulmatemoviecharacter-ws8313.koyeb.app/result/",
           axiosConfig
         );
         setUserMBTI(res.data.user_mbti);
@@ -36,7 +38,8 @@ const TestCompletedPage = () => {
     async function getWordCloud() {
       try {
         const res = await axios.get(
-          "https://soulmatemoviecharacter-ws8313.koyeb.app/result/top10",
+          "http://localhost:5000/result/top10",
+          // "https://soulmatemoviecharacter-ws8313.koyeb.app/result/top10",
           axiosConfig
         );
         setWordCloud(res.data);
@@ -50,54 +53,41 @@ const TestCompletedPage = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>영화 캐릭터 테스트</div>
+      <Header subtitle={"테스트가 완료되었습니다"} />
 
-      <div className={styles.subtitle}>테스트가 완료되었습니다</div>
+      <Main
+        src={wordCloud.word_cloud_src}
+        alt={wordCloud.word_cloud_src}
+        subtitle={userMBTI + " 유형의 워드 클라우드"}
+      />
 
-      <div className={styles.img_container}>
-        <img
-          className={styles.word_cloud}
-          src={wordCloud.word_cloud_src}
-          alt={wordCloud.word_cloud_src}
-        />
-        <div className={styles.text}>{userMBTI} 유형의 워드 클라우드</div>
-      </div>
-
-      <button
-        className={styles.btn}
+      <Button
+        content={"나와 같은 유형인 캐릭터 확인하기"}
         onClick={() => {
           history.push("/MbtiCharacterPage");
         }}
-      >
-        나와 같은 유형인 캐릭터 확인하기
-      </button>
+      />
 
-      <button
-        className={styles.btn}
+      <Button
+        content={"나와 궁합이 잘 맞는 캐릭터 확인하기"}
         onClick={() => {
           history.push("/MbtiCompatiblePage");
         }}
-      >
-        나와 궁합이 잘 맞는 캐릭터 확인하기
-      </button>
+      />
 
-      <button
-        className={styles.btn}
+      <Button
+        content={"같은 " + userMBTI + " 유형에게 인기있는 영화 확인하기"}
         onClick={() => {
           history.push("/MbtiTop10Page");
         }}
-      >
-        같은 {userMBTI} 유형에게 인기있는 영화 확인하기
-      </button>
+      />
 
-      <button
-        className={styles.btn}
+      <Button
+        content={"테스트 다시 하기"}
         onClick={() => {
           history.push("/");
         }}
-      >
-        테스트 다시 하기
-      </button>
+      />
     </div>
   );
 };
