465ca91545885e2a5bccd786c40272e952d4ba13
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-21 01:49:55 +0900

    Modify: components import Add characterList

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button/Button";
+export { default as CharacterList } from "./CharacterList/CharacterList";
 export { default as Header } from "./Header/Header";
 export { default as Loading } from "./Loading/Loading";
 export { default as Main } from "./Main/Main";
