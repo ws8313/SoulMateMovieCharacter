d1844de3cd84c2399c3df8ca1f92da7554500ea4
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-23 20:04:08 +0900

    refactor: MbtiCharacterPage

--- src/pages/MbtiCharacterPage.js
+++ src/pages/MbtiCharacterPage.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import styles from "./MbtiCharacterPage.module.css";
+import { Header, CharacterList, Button, Loading } from "../components";
 
 const MbtiCharacterPage = () => {
-  const [userMBTI, setUserMBTI] = useState("");
   const [charList, setCharList] = useState([]);
+  const [loading, setLoading] = useState(null);
 
   const history = useHistory();
+  const location = useLocation();
+
+  const userMBTI = location.state.userMBTI;
 
   const accessToken = sessionStorage.getItem("token");
 
@@ -21,99 +25,69 @@ const MbtiCharacterPage = () => {
   };
 
   useEffect(() => {
-    async function getMbti() {
-      try {
-        const mbti = await axios.get(
-          "https://soulmatemoviecharacter-ws8313.koyeb.app/result/",
-          axiosConfig
-        );
-        setUserMBTI(mbti.data.user_mbti);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getMbti();
-  }, [userMBTI]);
-
-  useEffect(() => {
-    async function getMbtiCharacter() {
-      try {
-        const res = await axios.get(
-          "https://soulmatemoviecharacter-ws8313.koyeb.app/character/0",
-          axiosConfig
-        );
-        setCharList(res.data.character_info);
-      } catch (error) {
-        console.log(error);
-      }
-    }
+    setLoading(true);
     getMbtiCharacter();
-  }, [userMBTI]);
+  }, []);
 
-  const refreshHandler = () => {
-    async function getMbtiCharacterRefresh() {
-      try {
-        const res = await axios.get(
-          "https://soulmatemoviecharacter-ws8313.koyeb.app/character/refresh/0",
-          axiosConfig
-        );
+  const getMbtiCharacter = async () => {
+    await axios
+      .get(
+        "http://localhost:5000/character/0",
+        // "https://soulmatemoviecharacter-ws8313.koyeb.app/character/0",
+        axiosConfig
+      )
+      .then((res) => {
         setCharList(res.data.character_info);
-      } catch (error) {
+        setLoading(false);
+      })
+      .catch((error) => {
         console.log(error);
-      }
-    }
-    getMbtiCharacterRefresh();
+      });
   };
 
-  const clickHandler = (idx) => {
-    history.push({
-      pathname: "/MbtiCharacterMovieListPage",
-      state: {
-        idx: idx,
-        charList: charList,
-      },
-    });
+  const refreshMbtiCharacter = async () => {
+    await axios
+      .get(
+        "http://localhost:5000/character/refresh/0",
+        // "https://soulmatemoviecharacter-ws8313.koyeb.app/character/refresh/0",
+        axiosConfig
+      )
+      .then((res) => {
+        setCharList(res.data.character_info);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const refreshHandler = () => {
+    setLoading(true);
+    refreshMbtiCharacter();
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>영화 캐릭터 테스트</div>
+      <Header
+        subtitle={"맘에 드는 캐릭터를 클릭해 보세요"}
+        description={"나와 같은 " + userMBTI + " 유형의 영화 속 캐릭터"}
+      />
 
-      <div className={styles.description}>맘에 드는 캐릭터를 클릭해 보세요</div>
+      <CharacterList characterList={charList} />
 
-      <div className={styles.subtitle}>
-        나와 같은 {userMBTI} 유형의 영화 속 캐릭터
-      </div>
+      <Button
+        content={"같은 유형의 다른 캐릭터 보기"}
+        onClick={refreshHandler}
+      />
 
-      <div className={styles.character_list}>
-        {charList &&
-          charList.map((item, idx) => {
-            return (
-              <div key={idx}>
-                <img
-                  className={styles.character_img}
-                  src={item[2]}
-                  alt={item[1] + " 사진"}
-                  onClick={() => clickHandler(idx)}
-                />
-                <div className={styles.character_name}>{item[1]}</div>
-              </div>
-            );
-          })}
-      </div>
-
-      <button className={styles.btn} onClick={refreshHandler}>
-        같은 유형의 다른 캐릭터 보기
-      </button>
-
-      <button
-        className={styles.btn}
+      <Button
+        content={"뒤로 가기"}
         onClick={() => {
           history.goBack();
         }}
-      >
-        뒤로 가기
-      </button>
+      />
+
+      {loading && <Loading />}
     </div>
   );
 };
