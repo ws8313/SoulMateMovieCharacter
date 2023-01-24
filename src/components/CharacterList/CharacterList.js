b145df23a2ad6d82487be55387c95fdc580aa2f1
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-24 17:15:08 +0900

    Modify: modify pathname

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import styles from "./CharacterList.module.css"
+import styles from "./CharacterList.module.css";
 
 const CharacterList = ({ characterList, compatibleMBTI }) => {
   const history = useHistory();
 
   const clickHandler = (idx) => {
     history.push({
-      pathname: compatibleMBTI ? "/MbtiCharacterMovieListPage" : "/MbtiCompatibleMovieListPage",
+      pathname: compatibleMBTI
+        ? "/MbtiCompatibleMovieListPage"
+        : "/MbtiCharacterMovieListPage",
       state: {
         idx: idx,
         charList: characterList,
@@ -17,22 +19,22 @@ const CharacterList = ({ characterList, compatibleMBTI }) => {
 
   return (
     <div className={styles.character_list}>
-        {characterList &&
-          characterList.map((item, idx) => {
-            return (
-              <div key={idx}>
-                <img
-                  className={styles.character_img}
-                  src={item[2]}
-                  alt={item[1] + " 사진"}
-                  onClick={() => clickHandler(idx)}
-                />
-                <div className={styles.character_name}>{item[1]}</div>
-              </div>
-            );
-          })}
-      </div>
-  )
-}
+      {characterList &&
+        characterList.map((item, idx) => {
+          return (
+            <div key={idx}>
+              <img
+                className={styles.character_img}
+                src={item[2]}
+                alt={item[1] + " 사진"}
+                onClick={() => clickHandler(idx)}
+              />
+              <div className={styles.character_name}>{item[1]}</div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
 export default CharacterList;
