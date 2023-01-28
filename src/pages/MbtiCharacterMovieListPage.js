fc4a29eab8698dc45d9b6d7ce1b2deafcbb4ed04
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-28 20:56:21 +0900

    refactor: MbtiCharacterMovieListPage

--- src/pages/MbtiCharacterMovieListPage.js
+++ src/pages/MbtiCharacterMovieListPage.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useHistory, useLocation } from "react-router-dom";
 import styles from "./MbtiCharacterMovieListPage.module.css";
+import { Header, MovieList, Button, Loading } from "../components";
 
 const MbtiCharacterMovieListPage = () => {
   const [movieList, setMovieList] = useState([]);
+  const [loading, setLoading] = useState(null);
 
   const history = useHistory();
   const location = useLocation();
@@ -27,36 +29,35 @@ const MbtiCharacterMovieListPage = () => {
   };
 
   useEffect(() => {
-    async function getCharacterMovieList() {
-      try {
-        const res = await axios.get(
-          `https://soulmatemoviecharacter-ws8313.koyeb.app/character/character_movie_list?character_id=${character_id}&character_name=${encodeURIComponent(
-            character_name
-          )}`,
-          axiosConfig
-        );
-        setMovieList(res.data.character_movies);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    getCharacterMovieList();
+    setLoading(true);
+    getMovieList();
   }, [character_id, character_name]);
 
-  const clickHandler = (items) => {
-    history.push({
-      pathname: "/MovieInfoPage",
-      state: {
-        movieInfos: items,
-      },
-    });
+  const getMovieList = async () => {
+    await axios
+      .get(
+        `http://localhost:5000/character/character_movie_list?character_id=${character_id}&character_name=${encodeURIComponent(
+          character_name
+        )}`,
+        // `https://soulmatemoviecharacter-ws8313.koyeb.app/character/character_movie_list?character_id=${character_id}&character_name=${encodeURIComponent(
+        //   character_name
+        // )}`,
+        axiosConfig
+      )
+      .then((res) => {
+        setMovieList(res.data.character_movies);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const logout = () => {
     axios
       .get(
-        "https://soulmatemoviecharacter-ws8313.koyeb.app/user/logout",
+        // "https://soulmatemoviecharacter-ws8313.koyeb.app/user/logout",
+        "http://localhost:5000/user/logout",
         axiosConfig
       )
       .then(() => {
@@ -69,65 +70,23 @@ const MbtiCharacterMovieListPage = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>영화 캐릭터 테스트</div>
+      <Header
+        subtitle={"맘에 드는 영화 포스터를 클릭해 보세요"}
+        description={character_name + " 등장한 영화"}
+      />
 
-      <div className={styles.description}>
-        맘에 드는 영화 포스터를 클릭해 보세요
-      </div>
+      <MovieList movieList={movieList} />
 
-      <div className={styles.subtitle}>{character_name + " 등장한 영화"}</div>
-
-      <div className={styles.movie_list_container}>
-        {movieList &&
-          movieList.map((items, idx) => {
-            return (
-              <div className={styles.movie_list} key={idx}>
-                <img
-                  className={styles.movie_img}
-                  src={items.image_link}
-                  alt={items.kor_title + " 포스터"}
-                  onClick={() => clickHandler(items)}
-                />
-                <div>
-                  <div className={styles.movie_title}>{items.kor_title}</div>
-                  <div className={styles.movie_info}>
-                    <div className={styles.movie_genre_container}>
-                      <div className={styles.movie_info_content}>장르</div>
-                      <div className={styles.movie_genre}>
-                        {items.genres.join(", ")}
-                      </div>
-                    </div>
-                    <div>
-                      <span className={styles.movie_info_content}>개봉</span>
-                      <span>{items.pub_year}년</span>
-                    </div>
-                    <div>
-                      <span className={styles.movie_info_content}>런타임</span>
-                      <span>{items.run_time}분</span>
-                    </div>
-                    <div>
-                      <span className={styles.movie_info_content}>평점</span>
-                      <span>{items.rating}</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-      </div>
-
-      <button
-        className={styles.btn}
+      <Button
+        content={"뒤로 가기"}
         onClick={() => {
           history.goBack();
         }}
-      >
-        뒤로 가기
-      </button>
+      />
 
-      <button className={styles.btn} onClick={logout}>
-        테스트 다시 하기
-      </button>
+      <Button content={"테스트 다시 하기"} onClick={logout} />
+
+      {loading && <Loading />}
     </div>
   );
 };
