504f682bc288f80deaa536febf5fc4708f8bcebd
Wooseong Kim <kimwooseong8313@gmail.com>
2023-01-31 20:06:57 +0900

    refactor: Change MainPage image from jpg to webp

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -19,7 +19,7 @@ const MainPage = () => {
       <div className={styles.container}>
         <Header subtitle={"당신과 똑같은 성향의 영화 캐릭터는?"} />
 
-        <Main src={"img/for_test/0.png"} alt={"main logo"} />
+        <Main src={"img/for_test/0.webp"} alt={"main logo"} />
 
         <Button content={"인생캐 알아보기"} onClick={openLoginModal} />
         <Button content={"바로 결과 보기"} onClick={openModal} />
