99824034cf464a4ff2dc58656875fee9207942fd
Wooseong Kim <kimwooseong8313@gmail.com>
2023-02-08 17:25:35 +0900

    delete: MbtiCompatibleMovieListPage

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ const MbtiCharacterMovieListPage = lazy(() =>
   import("./pages/MbtiCharacterMovieListPage")
 );
 const MbtiCompatiblePage = lazy(() => import("./pages/MbtiCompatiblePage"));
-const MbtiCompatibleMovieListPage = lazy(() =>
-  import("./pages/MbtiCompatibleMovieListPage")
-);
 const MbtiTop10Page = lazy(() => import("./pages/MbtiTop10Page"));
 const MovieInfoPage = lazy(() => import("./pages/MovieInfoPage"));
 
@@ -31,10 +28,6 @@ function App() {
           />
           <Route path="/MovieInfoPage" component={MovieInfoPage} />
           <Route path="/MbtiCompatiblePage" component={MbtiCompatiblePage} />
-          <Route
-            path="/MbtiCompatibleMovieListPage"
-            component={MbtiCompatibleMovieListPage}
-          />
           <Route path="/MbtiTop10Page" component={MbtiTop10Page} />
         </Switch>
       </Suspense>
